dc3989216bb6d8455091466b63260c5d6921ad95
saadi <salim301910@gmail.com>
2026-06-15 02:04:19 +0330

    minor fix

--- app/src/main/java/com/example/ui/components/Components.kt
+++ app/src/main/java/com/example/ui/components/Components.kt
@@ -7,6 +7,7 @@ import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
